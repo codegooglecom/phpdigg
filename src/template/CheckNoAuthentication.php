67708e7bb90ce5730e1b8c1b2a634e5b1fcd5fd6
bbbiao <bbbiao@bfce1677-6843-0410-b5f4-b360798c0c05>
2008-01-28 03:00:24 +0000

    Refactor using CodeIgniter

--- src/template/CheckNoAuthentication.php
+++
@@ -1,7 +0,0 @@
-<?php
-$username = $_COOKIE["userName"];
-
-if ($username != null) {
-	header("Location: default.php");
-}
-?>
--- src/template/DataTable.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "digg-item.php";
-$page = $_GET["page"] ? $_GET["page"] : 1;
-$itemCount = $action->itemCount();
-
-$order = $tab == "digg" ? "recommend" : "gmtCreate";
-
-$index_digg_item = $action->indexDiggItemWithAvator($page, 10, $order);
-?>
